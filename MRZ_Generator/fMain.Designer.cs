16bbe3c609bd8a74c6ff86374574e8f4886d50c8
Thanh Dinh <thanhdinhbao@outlook.com.vn>
2024-04-20 11:56:13 +0700

    Add project files.

---
+++ MRZ_Generator/fMain.Designer.cs
@@ -0,0 +1,359 @@
+﻿
+namespace MRZ_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.cbProvince = new System.Windows.Forms.ComboBox();
+            this.radMale = new System.Windows.Forms.RadioButton();
+            this.radFemale = new System.Windows.Forms.RadioButton();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtDOB = new System.Windows.Forms.DateTimePicker();
+            this.dtNgayCap = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(105, 34);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(185, 20);
+            this.txtFName.TabIndex = 1;
+            // 
+            // cbProvince
+            // 
+            this.cbProvince.FormattingEnabled = true;
+            this.cbProvince.Items.AddRange(new object[] {
+            "Hà Nội",
+            "Hà Giang",
+            "Cao Bằng",
+            "Bắc Kạn",
+            "Tuyên Quang",
+            "Lào Cai",
+            "Điện Biên",
+            "Lai Châu",
+            "Sơn La",
+            "Yên Bái",
+            "Hòa Bình",
+            "Thái Nguyên",
+            "Lạng Sơn",
+            "Quảng Ninh",
+            "Bắc Giang",
+            "Phú Thọ",
+            "Vĩnh Phúc",
+            "Bắc Ninh",
+            "Hải Dương",
+            "Hải Phòng",
+            "Hưng Yên",
+            "Thái Bình",
+            "Hà Nam",
+            "Nam Định",
+            "Ninh Bình",
+            "Thanh Hóa",
+            "Nghệ An",
+            "Hà Tĩnh",
+            "Quảng Bình",
+            "Quảng Trị",
+            "Thừa Thiên Huế",
+            "Đà Nẵng",
+            "Quảng Nam",
+            "Quảng Ngãi",
+            "Bình Định",
+            "Phú Yên",
+            "Khánh Hòa",
+            "Ninh Thuận",
+            "Bình Thuận",
+            "Kon Tum",
+            "Gia Lai",
+            "Đắk Lắk",
+            "Đắk Nông",
+            "Lâm Đồng",
+            "Bình Phước",
+            "Tây Ninh",
+            "Bình Dương",
+            "Đồng Nai",
+            "Bà Rịa - Vũng Tàu",
+            "Hồ Chí Minh",
+            "Long An",
+            "Tiền Giang",
+            "Bến Tre",
+            "Trà Vinh",
+            "Vĩnh Long",
+            "Đồng Tháp",
+            "An Giang",
+            "Kiên Giang",
+            "Cần Thơ",
+            "Hậu Giang",
+            "Sóc Trăng",
+            "Bạc Liêu",
+            "Cà Mau"});
+            this.cbProvince.Location = new System.Drawing.Point(105, 127);
+            this.cbProvince.Name = "cbProvince";
+            this.cbProvince.Size = new System.Drawing.Size(185, 21);
+            this.cbProvince.TabIndex = 3;
+            // 
+            // radMale
+            // 
+            this.radMale.AutoSize = true;
+            this.radMale.Checked = true;
+            this.radMale.Location = new System.Drawing.Point(128, 174);
+            this.radMale.Name = "radMale";
+            this.radMale.Size = new System.Drawing.Size(47, 17);
+            this.radMale.TabIndex = 4;
+            this.radMale.TabStop = true;
+            this.radMale.Text = "Nam";
+            this.radMale.UseVisualStyleBackColor = true;
+            // 
+            // radFemale
+            // 
+            this.radFemale.AutoSize = true;
+            this.radFemale.Location = new System.Drawing.Point(219, 174);
+            this.radFemale.Name = "radFemale";
+            this.radFemale.Size = new System.Drawing.Size(39, 17);
+            this.radFemale.TabIndex = 5;
+            this.radFemale.Text = "Nữ";
+            this.radFemale.UseVisualStyleBackColor = true;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(100, 347);
+            this.txtID.MaxLength = 10;
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(190, 20);
+            this.txtID.TabIndex = 8;
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(353, 108);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(87, 61);
+            this.btnGen.TabIndex = 9;
+            this.btnGen.Text = "Gen";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(27, 389);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(341, 144);
+            this.txtResult.TabIndex = 1;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(383, 462);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 34);
+            this.btnCopy.TabIndex = 10;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Họ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Địa chỉ:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Giới tính:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Ngày sinh:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Ngày hết hạn:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(66, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "ID:";
+            // 
+            // dtDOB
+            // 
+            this.dtDOB.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtDOB.Location = new System.Drawing.Point(105, 240);
+            this.dtDOB.MaxDate = new System.DateTime(2100, 12, 31, 0, 0, 0, 0);
+            this.dtDOB.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.dtDOB.Name = "dtDOB";
+            this.dtDOB.Size = new System.Drawing.Size(176, 20);
+            this.dtDOB.TabIndex = 6;
+            // 
+            // dtNgayCap
+            // 
+            this.dtNgayCap.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtNgayCap.Location = new System.Drawing.Point(105, 291);
+            this.dtNgayCap.MaxDate = new System.DateTime(2100, 12, 31, 0, 0, 0, 0);
+            this.dtNgayCap.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.dtNgayCap.Name = "dtNgayCap";
+            this.dtNgayCap.Size = new System.Drawing.Size(175, 20);
+            this.dtNgayCap.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 49);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(44, 79);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 13);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "Tên:";
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(105, 76);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(185, 20);
+            this.txtLName.TabIndex = 2;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(353, 207);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(87, 46);
+            this.btnExit.TabIndex = 31;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 568);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dtNgayCap);
+            this.Controls.Add(this.dtDOB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.radFemale);
+            this.Controls.Add(this.radMale);
+            this.Controls.Add(this.cbProvince);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtFName);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MRZ Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.ComboBox cbProvince;
+        private System.Windows.Forms.RadioButton radMale;
+        private System.Windows.Forms.RadioButton radFemale;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtDOB;
+        private System.Windows.Forms.DateTimePicker dtNgayCap;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ MRZ_Generator/fMain.cs
@@ -0,0 +1,354 @@
+﻿//Code by ThanhDinhBao
+//20/04/2024
+//https://www.facebook.com/thanhdinhbao.k20
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace MRZ_Generator
+{
+    public partial class Form1 : Form
+    {
+        List<Tuple<string, string>> provinces;
+        public Form1()
+        {
+            InitializeComponent();
+            provinces = new List<Tuple<string, string>>()
+            {
+                Tuple.Create("001","Hà Nội"),
+                Tuple.Create("002","Hà Giang"),
+                Tuple.Create("004","Cao Bằng"),
+                Tuple.Create("006","Bắc Kạn"),
+                Tuple.Create("008","Tuyên Quang"),
+                Tuple.Create("010","Lào Cai"),
+                Tuple.Create("011","Điện Biên"),
+                Tuple.Create("012","Lai Châu"),
+                Tuple.Create("014","Sơn La"),
+                Tuple.Create("015","Yên Bái"),
+                Tuple.Create("017","Hòa Bình"),
+                Tuple.Create("019","Thái Nguyên"),
+                Tuple.Create("020","Lạng Sơn"),
+                Tuple.Create("022","Quảng Ninh"),
+                Tuple.Create("024","Bắc Giang"),
+                Tuple.Create("025","Phú Thọ"),
+                Tuple.Create("026","Vĩnh Phúc"),
+                Tuple.Create("027","Bắc Ninh"),
+                Tuple.Create("030","Hải Dương"),
+                Tuple.Create("031","Hải Phòng"),
+                Tuple.Create("033","Hưng Yên"),
+                Tuple.Create("034","Thái Bình"),
+                Tuple.Create("035","Hà Nam"),
+                Tuple.Create("036","Nam Định"),
+                Tuple.Create("037","Ninh Bình"),
+                Tuple.Create("038","Thanh Hóa"),
+                Tuple.Create("040","Nghệ An"),
+                Tuple.Create("042","Hà Tĩnh"),
+                Tuple.Create("044","Quảng Bình"),
+                Tuple.Create("045","Quảng Trị"),
+                Tuple.Create("046","Thừa Thiên Huế"),
+                Tuple.Create("048","Đà Nẵng"),
+                Tuple.Create("049","Quảng Nam"),
+                Tuple.Create("051","Quảng Ngãi"),
+                Tuple.Create("052","Bình Định"),
+                Tuple.Create("054","Phú Yên"),
+                Tuple.Create("056","Khánh Hòa"),
+                Tuple.Create("058","Ninh Thuận"),
+                Tuple.Create("060","Bình Thuận"),
+                Tuple.Create("062","Kon Tum"),
+                Tuple.Create("064","Gia Lai"),
+                Tuple.Create("066","Đắk Lắk"),
+                Tuple.Create("067","Đắk Nông"),
+                Tuple.Create("068","Lâm Đồng"),
+                Tuple.Create("070","Bình Phước"),
+                Tuple.Create("072","Tây Ninh"),
+                Tuple.Create("074","Bình Dương"),
+                Tuple.Create("075","Đồng Nai"),
+                Tuple.Create("077","Bà Rịa - Vũng Tàu"),
+                Tuple.Create("079","Hồ Chí Minh"),
+                Tuple.Create("080","Long An"),
+                Tuple.Create("082","Tiền Giang"),
+                Tuple.Create("083","Bến Tre"),
+                Tuple.Create("084","Trà Vinh"),
+                Tuple.Create("086","Vĩnh Long"),
+                Tuple.Create("087","Đồng Tháp"),
+                Tuple.Create("089","An Giang"),
+                Tuple.Create("091","Kiên Giang"),
+                Tuple.Create("092","Cần Thơ"),
+                Tuple.Create("093","Hậu Giang"),
+                Tuple.Create("094","Sóc Trăng"),
+                Tuple.Create("095","Bạc Liêu"),
+                Tuple.Create("096","Cà Mau")
+            };
+            cbProvince.DataSource = provinces;
+            cbProvince.ValueMember = "Item1";
+            cbProvince.DisplayMember = "Item2";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtNgayCap.CustomFormat = "dd/MM/yyyy";
+            dtDOB.CustomFormat = "dd/MM/yyyy";
+        }
+
+        public string GetName()
+        {
+            string[] ten = txtLName.Text.Trim().ToUpper().Split(' ');
+            return txtFName.Text.Trim().ToUpper() + "<<" + string.Join("<", ten);
+        }
+
+        public string GetProvinceCode()
+        {
+            return cbProvince.SelectedValue.ToString();
+        }
+
+        public string GetSex()
+        {
+            string sex;
+            if (radMale.Checked)
+            {
+                sex = "M";
+            }
+            else
+            {
+                sex = "F";
+            }
+
+            return sex;
+
+        }
+
+        public string getDOB()
+        {
+            string date  = dtDOB.Value.ToString("yyMMdd");
+            return date;
+        }
+
+        public string getExpDate()
+        {
+            string date = dtNgayCap.Value.ToString("yyMMdd");
+            return date;
+        }
+        private static int GenDigit(string phrase)
+        {
+            int result = 0;
+            int count = 0;
+            int charValue = 0;
+
+            Dictionary<char, int> mrzCheckDigitLookup = new Dictionary<char, int>();
+
+            mrzCheckDigitLookup.Add('<', 0);
+            mrzCheckDigitLookup.Add('A', 10);
+            mrzCheckDigitLookup.Add('B', 11);
+            mrzCheckDigitLookup.Add('C', 12);
+            mrzCheckDigitLookup.Add('D', 13);
+            mrzCheckDigitLookup.Add('E', 14);
+            mrzCheckDigitLookup.Add('F', 15);
+            mrzCheckDigitLookup.Add('G', 16);
+            mrzCheckDigitLookup.Add('H', 17);
+            mrzCheckDigitLookup.Add('I', 18);
+            mrzCheckDigitLookup.Add('J', 19);
+            mrzCheckDigitLookup.Add('K', 20);
+            mrzCheckDigitLookup.Add('L', 21);
+            mrzCheckDigitLookup.Add('M', 22);
+            mrzCheckDigitLookup.Add('N', 23);
+            mrzCheckDigitLookup.Add('O', 24);
+            mrzCheckDigitLookup.Add('P', 25);
+            mrzCheckDigitLookup.Add('Q', 26);
+            mrzCheckDigitLookup.Add('R', 27);
+            mrzCheckDigitLookup.Add('S', 28);
+            mrzCheckDigitLookup.Add('T', 29);
+            mrzCheckDigitLookup.Add('U', 30);
+            mrzCheckDigitLookup.Add('V', 31);
+            mrzCheckDigitLookup.Add('W', 32);
+            mrzCheckDigitLookup.Add('X', 33);
+            mrzCheckDigitLookup.Add('Y', 34);
+            mrzCheckDigitLookup.Add('Z', 35);
+
+            foreach (var letter in phrase.Replace(' ', '<').ToUpper().ToCharArray())
+            {
+                count++;
+
+                if (Char.IsNumber(letter))
+                {
+                    charValue = (int)char.GetNumericValue(letter);
+                }
+                else if (mrzCheckDigitLookup.ContainsKey(letter))
+                {
+                    charValue = mrzCheckDigitLookup[letter];
+                }
+                else
+                {
+                    var ex = new Exception("Unknown character in MRZ :" + letter);
+                    throw ex;
+                }
+
+                var weightedcharValue = WeightingMultiplier(count, charValue);
+                result += weightedcharValue;
+                var tmp = " " + letter + "[" + charValue + "] * " + WeightingMultiplier(count, 1) + " = " + weightedcharValue + " += " + result;
+                if (count > 2)
+                {
+                    count = 0;
+                }
+                //tmp.Dump();
+            }
+
+            var remainder = result % 10;
+            //(result+" % 10 = "+remainder+Environment.NewLine).Dump();
+
+            return remainder;
+        }
+
+        private static int WeightingMultiplier(int count, int value)
+        {
+            switch (count)
+            {
+                case 1:
+                    value *= 7;
+                    break;
+                case 2:
+                    value *= 3;
+                    break;
+                case 3:
+                    value *= 1;
+                    break;
+            }
+
+            return value;
+        }
+
+        private Dictionary<string, string> ProvinceCode = new Dictionary<string, string>
+    {
+        {"Hà Nội","001"},
+        {"Hà Giang","002"},
+        {"Cao Bằng","004"},
+        {"Bắc Kạn","006"},
+        {"Tuyên Quang","008"},
+        {"Lào Cai","010"},
+        {"Điện Biên","011"},
+        {"Lai Châu","012"},
+        {"Sơn La","014"},
+        {"Yên Bái","015"},
+        {"Hòa Bình","017"},
+        {"Thái Nguyên","019"},
+        {"Lạng Sơn","020"},
+        {"Quảng Ninh","022"},
+        {"Bắc Giang","024"},
+        {"Phú Thọ","025"},
+        {"Vĩnh Phúc","026"},
+        {"Bắc Ninh","027"},
+        {"Hải Dương","030"},
+        {"Hải Phòng","031"},
+        {"Hưng Yên","033"},
+        {"Thái Bình","034"},
+        {"Hà Nam","035"},
+        {"Nam Định","036"},
+        {"Ninh Bình","037"},
+        {"Thanh Hóa","038"},
+        {"Nghệ An","040"},
+        {"Hà Tĩnh","042"},
+        {"Quảng Bình","044"},
+        {"Quảng Trị","045"},
+        {"Thừa Thiên Huế","046"},
+        {"Đà Nẵng","048"},
+        {"Quảng Nam","049"},
+        {"Quảng Ngãi","051"},
+        {"Bình Định","052"},
+        {"Phú Yên","054"},
+        {"Khánh Hòa","056"},
+        {"Ninh Thuận","058"},
+        {"Bình Thuận","060"},
+        {"Kon Tum","062"},
+        {"Gia Lai","064"},
+        {"Đắk Lắk","066"},
+        {"Đắk Nông","067"},
+        {"Lâm Đồng","068"},
+        {"Bình Phước","070"},
+        {"Tây Ninh","072"},
+        {"Bình Dương","074"},
+        {"Đồng Nai","075"},
+        {"Bà Rịa - Vũng Tàu","077"},
+        {"Hồ Chí Minh","079"},
+        {"Long An","080"},
+        {"Tiền Giang","082"},
+        {"Bến Tre","083"},
+        {"Trà Vinh","084"},
+        {"Vĩnh Long","086"},
+        {"Đồng Tháp","087"},
+        {"An Giang","089"},
+        {"Kiên Giang","091"},
+        {"Cần Thơ","092"},
+        {"Hậu Giang","093"},
+        {"Sóc Trăng","094"},
+        {"Bạc Liêu","095"},
+        {"Cà Mau","096"}
+    };
+
+        public class Utilities
+        {
+            public static void ResetAllControls(Control form)
+            {
+                foreach (Control control in form.Controls)
+                {
+                    if (control is TextBox)
+                    {
+                        TextBox textBox = (TextBox)control;
+                        textBox.Text = null;
+                    }
+
+                    if (control is ComboBox)
+                    {
+                        ComboBox comboBox = (ComboBox)control;
+                        if (comboBox.Items.Count > 0)
+                            comboBox.SelectedIndex = 0;
+                    }
+
+                    if (control is CheckBox)
+                    {
+                        CheckBox checkBox = (CheckBox)control;
+                        checkBox.Checked = false;
+                    }
+
+                    if (control is ListBox)
+                    {
+                        ListBox listBox = (ListBox)control;
+                        listBox.ClearSelected();
+                    }
+                }
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Utilities.ResetAllControls(this);
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtResult.Text);
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            //Gen final Hash
+            string f1 = txtID.Text + GenDigit(txtID.Text) + GetProvinceCode() + txtID.Text + "<<" + GenDigit(GetProvinceCode() + txtID.Text);
+            string f2 = getDOB() + GenDigit(getDOB()) + getExpDate() + GenDigit(getExpDate()) + "<<<<<<<<<<";
+            string fHash = GenDigit(f1 + f2).ToString();
+
+            //Gen each line
+            string line1 = "IDVNM" + txtID.Text + GenDigit(txtID.Text) + GetProvinceCode() + txtID.Text + "<<" + GenDigit(GetProvinceCode() + txtID.Text) + Environment.NewLine;
+            string line2 = getDOB() + GenDigit(getDOB()) + GetSex() + getExpDate() + GenDigit(getExpDate()) + "VNM" + "<<<<<<<<<<<" + fHash + Environment.NewLine;
+            int len = GetName().Length;
+            string line3 = GetName() + string.Concat(Enumerable.Repeat("<", 30 - len));
+
+            txtResult.Text = line1 + line2 + line3;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
